08d7e61fc36b9ff68075f7dd6008c0b8c1a7e0b3
Karol-Szalwinski <karol.szalwinski@gmail.com>
2017-02-12 20:31:40 +0100

    Pierwsza wersja

---
+++ src/AddressBookBundle/Controller/PersonController.php
@@ -0,0 +1,262 @@
+<?php
+
+namespace AddressBookBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+
+use AddressBookBundle\Entity\Address;
+use AddressBookBundle\Entity\Person;
+use AddressBookBundle\Entity\Email;
+use AddressBookBundle\Entity\Phone;
+
+class PersonController extends Controller {
+
+    /**
+     * @Route("/")
+     */
+    public function showIndexAction() {
+        $repo = $this->getDoctrine()->getRepository("AddressBookBundle:Person");
+        $persons = $repo->findAll();
+
+        return $this->render('AddressBookBundle:Person:show_index.html.twig', ["persons" => $persons]);
+    }
+
+    /**
+     * @Route("/{id}", requirements={"id" : "\d+"})
+     */
+    public function showPersonAction($id) {
+        $repo = $this->getDoctrine()->getRepository("AddressBookBundle:Person");
+        $person = $repo->find($id);
+        if ($person == null) {
+            throw $this->createNotFoundException();
+        }
+        return $this->render('AddressBookBundle:Person:show_person.html.twig', ["person" => $person]);
+    }
+
+    /**
+     * @Route("/new")
+     * @Method("GET")
+     */
+    public function newPersonAction() {
+        $person = new Person();
+        $action = $this->generateUrl('addressbook_person_newperson');
+        $formPerson = $this->generatePersonForm($person, $action);
+
+        return $this->render('AddressBookBundle:Person:new.html.twig', [
+                    "formPerson" => $formPerson->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/new")
+     * @Method("POST")
+     */
+    public function createPersonAction(Request $request) {
+        $person = new Person();
+        $form = $this->generatePersonForm($person, null);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() and $form->isValid()) {
+            $person = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($person);
+            $em->flush();
+            return $this->redirectToRoute('addressbook_person_showindex');
+        }
+        
+        
+        return $this->redirectToRoute('addressbook_person_newperson');
+    }
+
+    private function generatePersonForm($person, $action) {
+        $form = $this->createFormBuilder($person)
+                ->setAction($action)
+                ->add('name', 'text')
+                ->add('surname', 'text')
+                ->add('description', 'text')
+                ->add('save', 'submit', array('label' => 'Dodaj osobę'))
+                ->getForm();
+        return $form;
+    }
+
+    /**
+     * @Route("/{id}/modify" , requirements={"id" : "\d+"})
+     */
+    public function modifyAction(Request $request, $id) {
+        $personRepo = $this->getDoctrine()->getRepository("AddressBookBundle:Person");
+        $person = $personRepo->find($id);
+        $form = $this->generatePersonForm($person, null);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $request->getMethod() == 'POST' && $form->isValid()) {
+            $person = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($person);
+            $em->flush();
+            return $this->redirectToRoute('addressbook_person_showindex');
+        }
+        $action = $this->generateUrl('addressbook_person_createaddress', ["id" => $id]);
+        
+        $address = new Address();
+        $formAddress = $this->generateAddressForm($address, $action);
+        
+        $email = new Email();
+        $formEmail = $this->generateEmailForm($email, $action);
+        
+        $phone = new Phone();
+        $formPhone = $this->generatePhoneForm($phone, $action);
+
+        return $this->render('AddressBookBundle:Person:new.html.twig', [
+                    "formPerson" => $form->createView(),
+                    "formAddress" => $formAddress->createView(),
+                    "formEmail" => $formEmail->createView(),
+                    "formPhone" => $formPhone->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/delete" , requirements={"id" : "\d+"})
+     */
+    public function deleteAction($id) {
+        $personRepo = $this->getDoctrine()->getRepository("AddressBookBundle:Person");
+        $em = $this->getDoctrine()->getManager();
+        $personToDelete = $personRepo->find($id);
+        if ($personToDelete != null) {
+            $em->remove($personToDelete);
+            $em->flush();
+        }
+        return $this->redirectToRoute("addressbook_person_showindex");
+    }
+
+    /**
+     * @Route("{id}/addAddress", requirements={"id" : "\d+"})
+     * @Method("POST")
+     */
+    public function createAddressAction(Request $request, $id) {
+
+        $address = new Address();
+        $form = $this->generateAddressForm($address, null);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() and $form->isValid()) {
+            $repo = $this->getDoctrine()->getRepository("AddressBookBundle:Person");
+            $person = $repo->find($id);
+
+            $address = $form->getData();
+            $address->setPerson($person);
+
+            $person->addAddress($address);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($address);
+            $em->flush();
+        }
+        return $this->redirectToRoute('addressbook_person_modify', ["id" => $id]);
+    }
+    
+    /**
+     * @Route("{id}/addEmail", requirements={"id" : "\d+"})
+     * @Method("POST")
+     */
+    public function createEmailAction(Request $request, $id) {
+
+        $email = new Email();
+        $form = $this->generateEmailForm($email, null);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() and $form->isValid()) {
+            $repo = $this->getDoctrine()->getRepository("AddressBookBundle:Person");
+            $person = $repo->find($id);
+
+            $email = $form->getData();
+            $email->setPerson($person);
+
+            $person->addAddress($email);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($email);
+            $em->flush();
+        }
+        return $this->redirectToRoute('addressbook_person_modify', ["id" => $id]);
+    }
+    
+    /**
+     * @Route("{id}/addPhone", requirements={"id" : "\d+"})
+     * @Method("POST")
+     */
+    public function createPhoneAction(Request $request, $id) {
+
+        $phone = new Phone();
+        $form = $this->generateAddressForm($phone, null);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() and $form->isValid()) {
+            $repo = $this->getDoctrine()->getRepository("AddressBookBundle:Person");
+            $person = $repo->find($id);
+
+            $phone = $form->getData();
+            $phone->setPerson($person);
+
+            $person->addAddress($phone);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($phone);
+            $em->flush();
+        }
+        return $this->redirectToRoute('addressbook_person_modify', ["id" => $id]);
+    }  
+
+    private function generateAddressForm($address, $action) {
+        $form = $this->createFormBuilder($address)
+                ->setAction($action)
+                ->add('city', 'text')
+                ->add('street', 'text')
+                ->add('homeNumber', 'text')
+                ->add('localNumber', 'text')
+                ->add('save', 'submit', array('label' => 'Dodaj adres'))
+                ->getForm();
+        return $form;
+    }
+    private function generateEmailForm($email, $action) {
+        $form = $this->createFormBuilder($email)
+                ->setAction($action)
+                ->add('address', 'text')
+                ->add('type', 'number')
+                ->add('save', 'submit', array('label' => 'Dodaj email'))
+                ->getForm();
+        return $form;
+    }
+    private function generatePhoneForm($phone, $action) {
+        $form = $this->createFormBuilder($phone)
+                ->setAction($action)
+                ->add('number', 'text')
+                ->add('type', 'number')
+                ->add('save', 'submit', array('label' => 'Dodaj telefon'))
+                ->getForm();
+        return $form;
+    }
+    
+    
+    
+    
+    /**
+     * @Route("/add")
+     */
+    public function addAction() {
+        $repo = $this->getDoctrine()->getRepository("AddressBookBundle:Person");
+        $em = $this->getDoctrine()->getManager();
+        $person = $repo->find(3);
+
+        $address = new Address();
+        $address->setCity("New York");
+        $address->setStreet("Polna");
+        $address->setHomeNumber(10);
+        $address->setLocalNumber(99);
+        $address->setPerson($person);
+
+        $person->addAddress($address);
+
+        $em->persist($address);
+        $em->flush();
+        return $this->redirectToRoute("addressbook_person_showindex");
+    }
+
+}
---
+++ src/AddressBookBundle/Entity/Address.php~
@@ -0,0 +1,183 @@
+<?php
+
+namespace AddressBookBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Address
+ *
+ * @ORM\Table(name="address")
+ * @ORM\Entity(repositoryClass="AddressBookBundle\Repository\AddressRepository")
+ */
+class Address
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="city", type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="street", type="string", length=255)
+     */
+    private $street;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="home_number", type="string", length=255)
+     */
+    private $homeNumber;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="local_number", type="string", length=255)
+     */
+    private $localNumber;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Person", inversedBy="addresses")
+     * @ORM\JoinColumn(name="person_id", referencedColumnName="id")
+     */
+    private $person;
+    
+    
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     * @return Address
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string 
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set street
+     *
+     * @param string $street
+     * @return Address
+     */
+    public function setStreet($street)
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    /**
+     * Get street
+     *
+     * @return string 
+     */
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * Set homeNumber
+     *
+     * @param string $homeNumber
+     * @return Address
+     */
+    public function setHomeNumber($homeNumber)
+    {
+        $this->homeNumber = $homeNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get homeNumber
+     *
+     * @return string 
+     */
+    public function getHomeNumber()
+    {
+        return $this->homeNumber;
+    }
+
+    /**
+     * Set localNumber
+     *
+     * @param string $localNumber
+     * @return Address
+     */
+    public function setLocalNumber($localNumber)
+    {
+        $this->localNumber = $localNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get localNumber
+     *
+     * @return string 
+     */
+    public function getLocalNumber()
+    {
+        return $this->localNumber;
+    }
+
+    /**
+     * Set person
+     *
+     * @param \AddressBookBundle\Entity\Person $person
+     * @return Address
+     */
+    public function setPerson(\AddressBookBundle\Entity\Person $person = null)
+    {
+        $this->person = $person;
+
+        return $this;
+    }
+
+    /**
+     * Get person
+     *
+     * @return \AddressBookBundle\Entity\Person 
+     */
+    public function getPerson()
+    {
+        return $this->person;
+    }
+}
